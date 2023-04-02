13afd1660f84158264b9b6c73b43e3ed6c5ed7f2
ominyoung <ohmj090747@gmail.com>
2023-04-02 19:16:46 +0900

    :sparkles:Feat: 도입채팅 디비 생성

--- chats/admin.py
+++ chats/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from .models import Chat
-#
-# @admin.register(Chat)
-# class ChatAdmin(admin.ModelAdmin):
-#     pass
+from django.contrib import admin
+from .models import IntroChat
+
+@admin.register(IntroChat)
+class IntroChatAdmin(admin.ModelAdmin):
+    list_display = ['id', 'sentence', 'depth', 'link']
