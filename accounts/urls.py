073d43c68c07e616b652daafd60e0f3530a10b33
ominyoung <ohmj090747@gmail.com>
2023-04-07 14:41:20 +0900

    :sparkles:Feat: 새로운 기능 추가 - 닉네임 형식 저장

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from .views import UserAPI
+
+urlpatterns = [
+    path('', UserAPI.as_view())
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class UserAPI(APIView):
+
+    permission_classes = (AllowAny,)  # ToDo : 인증 구현후 IsAuthenticated 로변경
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
